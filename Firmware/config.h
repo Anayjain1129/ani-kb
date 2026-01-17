9e1be81f3670593a42b38d0dd5177a508a699078
Anni <anayjain1128@gmail.com>
2026-01-17 22:55:08 +0530

    Add files via upload

---
+++ Firmware/config.h
@@ -0,0 +1,13 @@
+/* Copyright 2026
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+/* Matrix configuration */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 14
